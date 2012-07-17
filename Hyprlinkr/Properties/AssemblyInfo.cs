c3878203a0fa813157558ad1f41b325dbdebcefe
Mark Seemann <mark@seemann.ms>
2012-07-17 16:36:07 +0200

    Updated version to 0.2.2,
    although renaming an interface is a breaking change, Semantic Versioning
    allows not incrementing the major version since the software is still in
    pre-release.

--- Hyprlinkr/Properties/AssemblyInfo.cs
+++ Hyprlinkr/Properties/AssemblyInfo.cs
@@ -33,7 +33,7 @@ using System;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.2.1")]
-[assembly: AssemblyFileVersion("0.2.1")]
+[assembly: AssemblyVersion("0.2.2")]
+[assembly: AssemblyFileVersion("0.2.2")]
 
 [assembly: CLSCompliant(true)]
