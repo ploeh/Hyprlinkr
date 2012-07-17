906d32b86ed088a215a5164ed6f1a7a6de3f3d46
Mark Seemann <mark@seemann.ms>
2012-07-17 14:52:25 +0200

    Added an AtomLinkModel class.

---
+++ ExampleService/AtomLinkModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Xml.Serialization;
+
+namespace Ploeh.Samples.Hyprlinkr.ExampleService
+{
+    [XmlType("link", Namespace = "http://www.w3.org/2005/Atom")]
+    public class AtomLinkModel
+    {
+        [XmlAttribute("href")]
+        public string Href { get; set; }
+
+        [XmlAttribute("rel")]
+        public string Rel { get; set; }
+    }
+}
