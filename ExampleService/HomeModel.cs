ba83711709c2d00f0d8462fba2978d25c8eec8c4
Mark Seemann <mark@seemann.ms>
2012-07-17 14:56:54 +0200

    Added HomeModel class.

---
+++ ExampleService/HomeModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Xml.Serialization;
+
+namespace Ploeh.Samples.Hyprlinkr.ExampleService
+{
+    [XmlRoot("home", Namespace = "http://www.ploeh.dk/hyprlinkr/sample/2012")]
+    public class HomeModel
+    {
+        [XmlArray("links")]
+        [XmlArrayItem("link", Namespace = "http://www.w3.org/2005/Atom")]
+        public AtomLinkModel[] Links { get; set; }
+
+        [XmlElement("name")]
+        public string Name { get; set; }
+    }
+}
