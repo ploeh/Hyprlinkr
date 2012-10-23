ce26f4232f34ac97b2126633d31f44b177b1478d
Daniel Hilgarth <d.hilgarth@fire-development.com>
2012-10-23 23:20:08 +0200

    Fixed comment formatting.

--- Hyprlinkr/Arg.cs
+++ Hyprlinkr/Arg.cs
@@ -1,12 +1,13 @@
 ﻿namespace Ploeh.Hyprlinkr
 {
-    /// <summary> Helper class to make it more visual that the parameters in the expression passed to <seealso cref="IActionVerifier.Verify{TController}"/> are
-    /// irrelevant. </summary>
+    /// <summary>
+    ///     Helper class to make it more visual that the parameters in the expression passed to <seealso cref="IActionVerifier.Verify{TController}"/> are irrelevant.
+    /// </summary>
     public static class Arg
     {
-        /// <summary> Returns a default value of the specified type. </summary>
-        /// <typeparam name="T"> The type to return the default value for. </typeparam>
-        /// <returns> default(T) </returns>
+        /// <summary>Returns a default value of the specified type.</summary>
+        /// <typeparam name="T">The type to return the default value for.</typeparam>
+        /// <returns>default(T)</returns>
         public static T OfType<T>()
         {
             return default(T);
