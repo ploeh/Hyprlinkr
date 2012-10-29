b1d3c0022ff19eb97530ce72d2454d5d358b2bad
Mark Seemann <mark@seemann.ms>
2012-10-29 21:32:36 +0100

    Added XML documentation to new interface method.

--- Hyprlinkr/IRouteDispatcher.cs
+++ Hyprlinkr/IRouteDispatcher.cs
@@ -43,6 +43,22 @@ namespace Ploeh.Hyprlinkr
         /// </remarks>
         Rouple Dispatch(MethodInfo method, IDictionary<string, object> routeValues);
 
+        /// <summary>
+        /// Provides dispatch information based on an Action Method.
+        /// </summary>
+        /// <param name="method">The method expression.</param>
+        /// <param name="routeValues">Route values.</param>
+        /// <returns>
+        /// An object containing the route name, as well as the route values.
+        /// </returns>
+        /// <remarks>
+        /// <para>
+        /// Note to implementers: Pass <paramref name="routeValues" /> through
+        /// to the return value if you don't modify it. However, if you wish to
+        /// add or remove values from the dictionary, you should create a copy
+        /// and mutate that copy, leaving the input unmodified.
+        /// </para>
+        /// </remarks>
         Rouple Dispatch(
             MethodCallExpression method,
             IDictionary<string, object> routeValues);
