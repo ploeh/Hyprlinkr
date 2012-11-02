9e9bc6e0e9c8e321579ecf27898976956010effd
Daniel Hilgarth <d.hilgarth@fire-development.com>
2012-11-02 08:13:06 +0100

    Removed unneeded namespaces.

--- Hyprlinkr.UnitTest/Controllers/DerivedController.cs
+++ Hyprlinkr.UnitTest/Controllers/DerivedController.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Ploeh.Hyprlinkr.UnitTest.Controllers
+﻿namespace Ploeh.Hyprlinkr.UnitTest.Controllers
 {
     public class DerivedController : BaseController
     {
