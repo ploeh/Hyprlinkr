ed1ac8ccea9b70dafbd24d432c5f3c7ece9f92bf
Mark Seemann <mark@seemann.ms>
2012-12-07 14:39:28 +0100

    Introduced the concept of an route values query.
    Doesn't do anything yet, though.

---
+++ Hyprlinkr.UnitTest/DefaultRouteValuesQueryTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit.Extensions;
+using Ploeh.Hyprlinkr;
+using Ploeh.AutoFixture.Idioms;
+using Xunit;
+
+namespace Ploeh.Hyprlinkr.UnitTest
+{
+    public class DefaultRouteValuesQueryTest
+    {
+        [Theory, AutoHypData]
+        public void SutHasAppropriateGuards(GuardClauseAssertion assertion)
+        {
+            assertion.Verify(typeof(DefaultRouteValuesQuery));
+        }
+
+        [Theory, AutoHypData]
+        public void SutIsRouteValuesQuery(DefaultRouteValuesQuery sut)
+        {
+            Assert.IsAssignableFrom<IRouteValuesQuery>(sut);
+        }
+    }
+}
