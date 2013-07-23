738274de5ded0e311a05594ee291fa6471d13517
Mark Seemann <mark@ploeh.dk>
2013-07-23 09:19:35 +0200

    Added assembly-level attributes to F# test project

---
+++ Hyprlinkr.UnitTest.FSharp/AssemblyInfo.fs
@@ -0,0 +1,16 @@
+﻿module Hyprlinkr.UnitTest.FSharp.AssemblyInfo
+
+open System.Reflection
+
+[<assembly: AssemblyTitle("Hyprlinkr.UnitTest.FSharp")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("Mark Seemann")>]
+[<assembly: AssemblyProduct("Hyprlinkr.UnitTest.FSharp")>]
+[<assembly: AssemblyCopyright("Copyright © Mark Seemann 2012")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+[<assembly: AssemblyVersion("0.7.0")>]
+[<assembly: AssemblyFileVersion("0.7.0")>]
+do()
