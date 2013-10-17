1163e3fa1769030c14c11eea024a824a634d2bae
Mark Seemann <mark@ploeh.dk>
2013-10-17 22:30:49 +0200

    Incremented minor version,
    in order to indicate that support for async Controllers is a new feature.

--- Hyprlinkr.UnitTest.FSharp/AssemblyInfo.fs
+++ Hyprlinkr.UnitTest.FSharp/AssemblyInfo.fs
@@ -11,6 +11,6 @@ open System.Reflection
 [<assembly: AssemblyTrademark("")>]
 [<assembly: AssemblyCulture("")>]
 
-[<assembly: AssemblyVersion("0.8.2")>]
-[<assembly: AssemblyFileVersion("0.8.2")>]
+[<assembly: AssemblyVersion("0.9.0")>]
+[<assembly: AssemblyFileVersion("0.9.0")>]
 do()
