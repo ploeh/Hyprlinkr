e219f8aca5069d17391faaf5410de02e8ccdef9d
Mark Seemann <mark@ploeh.dk>
2014-01-12 23:14:47 +0100

    Inlined GetBodyMethodCallExpression,
    since it was only used at a single call site.

--- Hyprlinkr/ExpressionExtensions.cs
+++ Hyprlinkr/ExpressionExtensions.cs
@@ -10,33 +10,6 @@ namespace Ploeh.Hyprlinkr
     internal static class ExpressionExtensions
     {
         /// <summary>
-        /// Gets the <see cref="MethodCallExpression"/> of the body of the supplied expression.
-        /// </summary>
-        /// <param name="expression">
-        /// The expression.
-        /// </param>
-        /// <returns>
-        /// The <see cref="MethodCallExpression"/> of the body of the supplied expression.
-        /// </returns>
-        /// <exception cref="System.ArgumentNullException"><paramref name="expression"/> is <c>null</c>.</exception>
-        /// <exception cref="System.ArgumentException"><paramref name="expression"/> doesn't represent an Action that invokes a method.</exception>
-        private static MethodCallExpression GetBodyMethodCallExpression(this LambdaExpression expression)
-        {
-            if (expression == null)
-                throw new ArgumentNullException("expression");
-
-            var methodCallExpression = expression.Body as MethodCallExpression;
-            if (methodCallExpression == null)
-            {
-                throw new ArgumentException(
-                    "The expression's body must be a MethodCallExpression. The code block supplied should invoke a method.\nExample: x => x.Foo().",
-                    "expression");
-            }
-
-            return methodCallExpression;
-        }
-
-        /// <summary>
         /// Gets the <see cref="MethodInfo"/> object of the body of the supplied expression.
         /// </summary>
         /// <param name="expression">
@@ -50,7 +23,15 @@ namespace Ploeh.Hyprlinkr
             if (expression == null)
                 throw new ArgumentNullException("expression");
 
-            return expression.GetBodyMethodCallExpression().Method;
+            var methodCallExpression = expression.Body as MethodCallExpression;
+            if (methodCallExpression == null)
+            {
+                throw new ArgumentException(
+                    "The expression's body must be a MethodCallExpression. The code block supplied should invoke a method.\nExample: x => x.Foo().",
+                    "expression");
+            }
+
+            return methodCallExpression.Method;
         }
     }
 }
