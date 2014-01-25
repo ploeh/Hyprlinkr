a78afda563d27d958e3870ca3b5d3ca6eabe8d68
Mark Seemann <mark@ploeh.dk>
2014-01-25 21:02:21 +0100

    Incremented patch version,
    in order to indicate that this resolves a bug, but adds no new features.

--- Hyprlinkr.UnitTest.FSharp/AssemblyInfo.fs
+++ Hyprlinkr.UnitTest.FSharp/AssemblyInfo.fs
@@ -11,6 +11,6 @@ open System.Reflection
 [<assembly: AssemblyTrademark("")>]
 [<assembly: AssemblyCulture("")>]
 
-[<assembly: AssemblyVersion("1.0.0.0")>]
-[<assembly: AssemblyFileVersion("1.0.0.0")>]
+[<assembly: AssemblyVersion("1.0.1.0")>]
+[<assembly: AssemblyFileVersion("1.0.1.0")>]
 do()
