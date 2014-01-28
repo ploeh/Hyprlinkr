64148d8a4da569c49704e5b8d46f30d5b3162f03
Mark Seemann <mark@ploeh.dk>
2014-01-28 13:34:47 +0100

    Updated build scripts to work with VS 2013.

--- Build-Release.ps1
+++ Build-Release.ps1
@@ -1,1 +1,9 @@
-.$env:windir\Microsoft.NET\Framework\v4.0.30319\MSBuild BuildRelease.msbuild /property:Configuration=Release
+function Get-VsVersion ()
+{
+    if ($env:VS120COMNTOOLS -ne $null)
+    {
+        " /property:VisualStudioVersion=12.0"
+    }
+}
+
+Start-Process -NoNewWindow -Wait -FilePath $env:windir\Microsoft.NET\Framework\v4.0.30319\MSBuild -ArgumentList ('BuildRelease.msbuild', '/property:Configuration=Release', (Get-VsVersion))
