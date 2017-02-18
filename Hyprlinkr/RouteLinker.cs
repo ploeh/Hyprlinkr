650d910d35774b68fc82d3ccdc3453e2fce58de2
Mark Seemann <mark@ploeh.dk>
2017-02-18 11:26:06 +0100

    Used UrlHelper.Link instead of UrlHelper.Route
    I'm not entirely sure why I didn't originally do this, but perhaps
    UrlHelper.Link wasn't available in ASP.NET Web API 1. If it was, it must
    have been my own ignorance.

--- Hyprlinkr/RouteLinker.cs
+++ Hyprlinkr/RouteLinker.cs
@@ -342,46 +342,32 @@ namespace Ploeh.Hyprlinkr
             return Task.Factory.StartNew(() => this.GetUri(methodCallExp));
         }
 
-        private Uri GetUri(MethodCallExpression methodCallExp)
-        {
-            var r = this.Dispatch(methodCallExp);
-
-            var relativeUri = this.GetRelativeUri(r);
-            var baseUri = this.GetBaseUri();
-            return new Uri(baseUri, relativeUri);
-        }
-
-        private Rouple Dispatch(MethodCallExpression methodCallExp)
-        {
-            var routeValues = this.valuesQuery.GetRouteValues(methodCallExp);
-            return this.dispatcher.Dispatch(methodCallExp, routeValues);
-        }
-
         #region CA suppressions
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA2204:Literals should be spelled correctly", MessageId = "GetUri", Justification = "Workaround for a bug in CA: https://connect.microsoft.com/VisualStudio/feedback/details/521030/")]
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA2204:Literals should be spelled correctly", MessageId = "IDictionary", Justification = "Workaround for a bug in CA: https://connect.microsoft.com/VisualStudio/feedback/details/521030/")]
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA2204:Literals should be spelled correctly", MessageId = "UrlHelper", Justification = "Workaround for a bug in CA: https://connect.microsoft.com/VisualStudio/feedback/details/521030/")]
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA2204:Literals should be spelled correctly", MessageId = "RouteLinker", Justification = "Workaround for a bug in CA: https://connect.microsoft.com/VisualStudio/feedback/details/521030/")]
         #endregion
-        private Uri GetRelativeUri(Rouple r)
+        private Uri GetUri(MethodCallExpression methodCallExp)
         {
+            var r = this.Dispatch(methodCallExp);
+
             var urlHelper = this.CreateUrlHelper();
-            var relativeUri = urlHelper.Route(r.RouteName, r.RouteValues);
-            if (relativeUri == null)
+            var link = urlHelper.Link(r.RouteName, r.RouteValues);
+            if (link == null)
                 throw new InvalidOperationException(
                     string.Format(
                         CultureInfo.CurrentCulture,
                         "The route string returned by System.Web.Http.Routing.UrlHelper.Route(string, IDictionary<string, object>) is null, which indicates an error. This can happen if the Action Method identified by the RouteLinker.GetUri method doesn't have a matching route with the name \"{0}\", or if the route parameter names don't match the method arguments.",
                         r.RouteName));
 
-            return new Uri(relativeUri, UriKind.Relative);
+            return new Uri(link);
         }
 
-        private Uri GetBaseUri()
+        private Rouple Dispatch(MethodCallExpression methodCallExp)
         {
-            var authority =
-                this.request.RequestUri.GetLeftPart(UriPartial.Authority);
-            return new Uri(authority);
+            var routeValues = this.valuesQuery.GetRouteValues(methodCallExp);
+            return this.dispatcher.Dispatch(methodCallExp, routeValues);
         }
 
         private UrlHelper CreateUrlHelper()
